a0ceed31c9740aa3647e09fa5ca54bd7405b2750
Sanjana S <sanjanasanju2201p@gmail.com>
2026-03-29 17:35:00 +0530

    three sum

--- DSA/Two pointers/Threesum.java
+++ DSA/Two pointers/Threesum.java
@@ -1,29 +1,51 @@
+import java.util.*;
 class Threesum{
-    public static int threeSum(int a[]){
-        int i=0; int n=a.length;
-        int j=i+1;  int sum=0;
-        int k=n-1;
-        for(i=0;i<n;i++){
-            while(j<k){
-                sum=a[j]+a[i]+a[k];
+    public static List<List<Integer>> threeSum(int a[]){
+        int n=a.length;
+        List<List<Integer>> result=new ArrayList<>();
+        Arrays.sort(a);
+        for(int i=0;i<a.length-2;i++){
+            if(i>0 && a[i]==a[i-1]){
+                continue;
+            }
+            int left=i+1;
+            int right=n-1;
+
+            while(left<right){
+                int sum = a[i]+a[left]+a[right];
                 if(sum==0){
-                    return sum;
-                else if (sum>0){
-                    k--;
+                    result.add(Arrays.asList(a[i],a[left],a[right]));
+                    while(left<right&&a[left]==a[left+1])left++;
+                    while(left<right&&a[right]==a[right-1])right--;
+                    left++;
+                    right--;
+                }
+                else if(sum>0){
+                    right--;
+
+                }
                 else{
-                    j++;
+                    left++;
                 }
 
+                }
+
+        
+            }
+            
+             return result;
                 
             }
 
-                }
-            }
-            }
-        }
+                
+            
         public static void main(String[] args) {
-            int a[]={-1,0,1,2,3};
-            threeSum(a);
+            int a[]={-1,0,1,2,-1,-4};
+            List<List<Integer>> ans=threeSum(a);
+            System.out.println(ans);
+
+
+
 
         }
     }
