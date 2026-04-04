1a318ac7ff05fe87bd6bcd8e2da82a556ff62da4
Sanjana S <sanjanasanju2201p@gmail.com>
2026-04-04 19:07:24 +0530

    class and objects

--- OOPS/Student.java
+++ OOPS/Student.java
@@ -9,19 +9,32 @@ class Student{
         System.out.println(marks);
         System.out.println(rollno);
     }
+    boolean isPass(){
+        if(marks>40){
+            return true;
+
+        }
+        else{
+            return false;
+        }
+
+    }
+    
+
     public static void main(String[] args) {
         Student s1=new Student();
         s1.name="Sanju";
-        s1.marks= 100;
-        s1.rollno= 58;
+        s1.marks= 40;
+        s1.rollno= 32;
         s1.displayInfo();
         Student s2=s1;
-        s2.name="anu";
+        s2.name="anu";  // Reference coping
         System.out.println(s1.name);
+        System.out.println("pass"+s1.isPass());
 
 
 
         
     }
-
 }
+
