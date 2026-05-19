03a33d5d52d777975c43859ac9b5e64ac2a76523
Sanjana S <sanjanasanju2201p@gmail.com>
2026-05-19 20:32:35 +0530

    miniproject

---
+++ OOPS/MiniProjects/ShapeCalculator/Circle.java
@@ -0,0 +1,16 @@
+package Preparation.OOPS.MiniProjects.ShapeCalculator;
+
+public class Circle extends Shape {
+    super(rad);
+    @Override
+    public void area(){
+        double area=3.14*rad*rad;
+        System.out.println(area);
+        
+    }
+    public void perimeter(){
+
+    }
+    
+    
+}
