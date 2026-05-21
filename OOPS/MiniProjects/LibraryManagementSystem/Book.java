6ff6167a9c1e51d120225d0f88478283b892d1ac
Sanjana S <sanjanasanju2201p@gmail.com>
2026-05-21 22:13:27 +0530

    updated ShapeCalculator

---
+++ OOPS/MiniProjects/LibraryManagementSystem/Book.java
@@ -0,0 +1,17 @@
+package Preparation.OOPS.MiniProjects.LibraryManagementSystem;
+
+public class Book {
+    private String Name;
+    private String Author;
+    private int ISBN;
+    private boolean isavailable;
+    public Book(String Name,String Author,int ISBN,boolean isavailable){
+        this.Name=Name;
+        this.Author=Author;
+        this.ISBN=ISBN;
+        this.isavailable=isavailable;
+    }
+    
+
+    
+}
--- OOPS/MiniProjects/ShapeCalculator/main.java
+++ OOPS/MiniProjects/ShapeCalculator/main.java
@@ -7,11 +7,11 @@ public class main {
     shapes[0]=new Circle(7);
     shapes[1]=new Rectangle(4.0,3.0,2.0);
     shapes[2]=new Triangle(4,3,2,4,6);
-    for(Shape s:shapes){
-        s.area();
-        s.perimeter();
-    }
-        
+    for (Shape s : shapes) {
+    s.area();
+    s.perimeter();
+}
+
     }
     
 }
